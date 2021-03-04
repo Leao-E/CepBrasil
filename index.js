cc50433a308b231ed60bcc900c0aceee7062b76c
Leao-E <eemanuelleao@gmail.com>
2021-03-04 09:17:52 -0300

    Finished v.1

--- index.js
+++ index.js
@@ -1,18 +1,56 @@
-const csvToSql = require('./src/csv-to-sql.js');
+const CsvToSql = require('./src/csv-to-sql.js');
+const process = require('process');
 const path = require('path');
 
-const inputPaths = {
-   acre: [
-      path.resolve('assets/data/acre/ac_1.csv'),
-      path.resolve('assets/data/acre/ac_2.csv'),
-      path.resolve('assets/data/acre/ac_3.csv'),
-      path.resolve('assets/data/acre/ac_4.csv'),
-      path.resolve('assets/data/acre/ac_5.csv'),
+const stateInitial = new Map([
+   ['acre', 'ac'],
+   ['alagoas', 'al'],
+   ['amapa', 'ap'],
+   ['amazonas', 'am'],
+   ['bahia', 'ba'],
+   ['ceara', 'ce'],
+   ['distrito_federal', 'df'],
+   ['espirito_santo', 'es'],
+   ['goias', 'go'],
+   ['mato_grosso', 'mt'],
+   ['mato_grosso_do_sul', 'ms'],
+   ['minas_gerais', 'mg'],
+   ['para', 'pa'],
+   ['paraiba', 'pb'],
+   ['pernambuco', 'pe'],
+   ['piaui', 'pi'],
+   ['rio_grande_do_sul', 'rs'],
+   ['rondonia', 'ro'],
+   ['santa_catarina', 'sc'],
+   ['sao_paulo', 'sp'],
+   ['sergipe', 'se'],
+]);
+
+function start(state, outputFile, dbName) {
+   const outputPath = path.resolve(`result/${outputFile ? outputFile : state}.sql`);
+
+   const inputPaths = [
+      path.resolve(`assets/data/${state}/${stateInitial.get(state)}_1.csv`),
+      path.resolve(`assets/data/${state}/${stateInitial.get(state)}_2.csv`),
+      path.resolve(`assets/data/${state}/${stateInitial.get(state)}_3.csv`),
+      path.resolve(`assets/data/${state}/${stateInitial.get(state)}_4.csv`),
+      path.resolve(`assets/data/${state}/${stateInitial.get(state)}_5.csv`),
    ]
+
+   CsvToSql.build(inputPaths, outputPath, dbName ? dbName : 'states_cep');
 }
 
-const outputPath = path.resolve('teste.sql');
-csvToSql.call(inputPaths.acre[0], outputPath, rec => {
-   const [cep, logradouro, complemento, bairro, idCidade, idEstado] = rec;
-   return `("${cep}", "${logradouro}", "${complemento}", "${bairro}", ${Number.parseInt(idCidade)}, ${Number.parseInt(idEstado)}),\n`;
-});
+console.log(process.argv);
+const getArg = argName => {
+   const rgxMatch = new RegExp(`--${argName}=.*`);
+   const rgxReplace = new RegExp(`--${argName}=`);
+
+   const value = process.argv.find(str => str.match(rgxMatch));
+   return value ? value.replace(rgxReplace, '') : false;
+}
+
+const state = getArg('state');
+const outputFile = getArg('outputFile');
+const dbName = getArg('dbName');
+
+start(state, outputFile, dbName);
--- src/csv-to-sql.js
+++ src/csv-to-sql.js
@@ -1,17 +1,31 @@
 const parse = require('csv-parse');
 const fs = require('fs');
 
-async function call(inputFile, outputFile, appendCallback = rec => rec){
+async function makeSqlInsertData(inputFile, outputFile, appendCallback = rec => rec){
    const parser = fs.createReadStream(inputFile).pipe(parse({
       delimiter: ",",
       trim: true
    }));
    for await (const record of parser) {
-      fs.appendFile(outputFile, appendCallback(record), err => {
-         if (err) throw err;
-         console.log(`error appending to file: ${outputFile}; data: ${record}`);
-      });
+      fs.appendFileSync(outputFile, appendCallback(record));
    }
 }
 
-exports.call = call;
+function makeSqlInsertQuery(outputFile, dbName){
+   const sqlQuery = `INSERT INTO ${dbName}\n(cep, logradouro, complemento, bairro, id_cidade, id_estado)\nVALUES\n`;
+
+   fs.appendFileSync(outputFile, sqlQuery);
+}
+
+function build (inputFiles, outputFile, dbName){
+   makeSqlInsertQuery(outputFile, dbName);
+
+   inputFiles.forEach(inputFile => makeSqlInsertData(inputFile, outputFile, rec => {
+      const [cep, logradouro, complemento, bairro, idCidade, idEstado] = rec;
+      return `("${cep}", "${logradouro}", "${complemento}", "${bairro}", ${Number.parseInt(idCidade)}, ${Number.parseInt(idEstado)}),\n`;
+   }));
+
+}
+
+
+exports.build = build;
